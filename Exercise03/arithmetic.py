d521f3de15bed1ea855703c471779b0b12463416
Heather Bryant <heather@rebelcompass.com>
2015-01-06 11:14:56 -0800

    Completed Exercise03

--- Exercise03/arithmetic.py
+++ Exercise03/arithmetic.py
@@ -1,31 +1,31 @@
 def add(first, second):
-    print "DEBUG MESSAGE: this is a dummy add function"
-    return 0
+    #print "DEBUG MESSAGE: this is a dummy add function"
+    return first + second
 
 def subtract(first, second):
-    print "DEBUG MESSAGE: this is a dummy subtract function"
-    return 0
+    #print "DEBUG MESSAGE: this is a dummy subtract function"
+    return first - second
 
 def multiply(first, second):
-    print "DEBUG MESSAGE: this is a dummy multiply function"
-    return 0
+    #print "DEBUG MESSAGE: this is a dummy multiply function"
+    return first * second
 
 def divide(first, second):
-    print "DEBUG MESSAGE: this is a dummy divide function"
-    return 0.0
+    #print "DEBUG MESSAGE: this is a dummy divide function"
+    return float(first)/float(second)
 
 def square(i):
-    print "DEBUG MESSAGE: this is a dummy square function"
-    return 0
+    #print "DEBUG MESSAGE: this is a dummy square function"
+    return i ** 2
 
 def cube(i):
-    print "DEBUG MESSAGE: this is a dummy cube function"
-    return 0
+    #print "DEBUG MESSAGE: this is a dummy cube function"
+    return i ** 3
 
 def mod(i, j):
-    print "DEBUG MESSAGE: this is a dummy mod function"
-    return 0
+    #print "DEBUG MESSAGE: this is a dummy mod function"
+    return i % j
 
 def power(i, j):
-    print "DEBUG MESSAGE: this is a dummy power function"
-    return 0
+    #print "DEBUG MESSAGE: this is a dummy power function"
+    return i ** j
--- Exercise03/calculator.py
+++ Exercise03/calculator.py
@@ -4,12 +4,51 @@ calculator.py
 Using our arithmetic.py file from Exercise02, create the
 calculator program yourself in this file.
 """
-
+from arithmetic import add, subtract, multiply, divide, square, cube, mod, power
 
 def main():
 	# Your code goes here
-	pass
+	
+	print "Enter your math problem in prefix notation. For example + 1 2. Enter q to quit."
 
+	while True:	
+		
+		try:
+			mathproblem = raw_input("> ")
+
+			tokens = mathproblem.split(" ")
+
+			if tokens[0] == "q":
+				break
+
+			else:
+
+				if tokens[0] == "+":
+					print(add(int(tokens[1]),(int(tokens[2]))))
+
+				elif tokens[0] == "-":
+					print(subtract(int(tokens[1]),(int(tokens[2]))))
+
+				elif tokens[0] == "*":
+					print(multiply(int(tokens[1]),(int(tokens[2]))))
+
+				elif tokens[0] == "/":
+					print(divide(int(tokens[1]),(int(tokens[2]))))
+
+				elif tokens[0] == "square":
+					print(square(int(tokens[1])))
+
+				elif tokens[0] == "cube":
+					print(cube(int(tokens[1])))
+
+				elif tokens[0] == "mod":
+					print(mod(int(tokens[1]),(int(tokens[2]))))
+
+				elif tokens[0] == "power":
+					print(power(int(tokens[1]),(int(tokens[2]))))
+
+		except ValueError:
+			print "I don't understand. Please enter a valid math problem or press q to quit."
 
 if __name__ == '__main__':
     main()
