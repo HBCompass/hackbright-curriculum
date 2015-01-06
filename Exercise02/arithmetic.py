61635e416d13bafea0beeda917d3fe157b3a5ad4
Heather Bryant <heather@rebelcompass.com>
2015-01-05 16:29:35 -0800

    Completed Exercise 02

--- Exercise02/arithmetic.py
+++ Exercise02/arithmetic.py
@@ -1,23 +1,23 @@
 def add(num1, num2):
-    return 10
+    return num1 + num2
 
 def subtract(num1, num2):
-    pass
+    return num1 - num2
 
 def multiply(num1, num2):
-    pass
+    return num1 * num2
 
 def divide(num1, num2):
-    pass
+    return float(num1) / float(num2)
 
 def square(num1):
-    pass
+    return num1 ** 2
 
 def cube(num1):
-    pass
+    return num1 ** 3
 
 def power(num1, num2):
-    pass
+    return num1 ** num2
 
 def mod(num1, num2):
-    pass
+    return num1 % num2
